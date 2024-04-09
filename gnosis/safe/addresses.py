b972bc4d42078975542413f66d534d9ec096b0ab
nick8319 <zhiltsov.nick@gmail.com>
2024-04-09 11:31:01 +0500

    feat: add zilliqa evm v1.3.0 contract addresses

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -869,6 +869,14 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 657757, "1.3.0+L2"),
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 657761, "1.3.0"),
     ],
+    EthereumNetwork.ZILLIQA_EVM: [
+        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 3702347, "1.3.0+L2"),
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 3702349, "1.3.0"),
+    ],
+    EthereumNetwork.ZILLIQA_EVM_TESTNET: [
+        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 6806700, "1.3.0+L2"),
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 6806702, "1.3.0"),
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -1411,5 +1419,11 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     ],
     EthereumNetwork.LISK_SEPOLIA_TESTNET: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 657735),  # v1.3.0
+    ],
+    EthereumNetwork.ZILLIQA_EVM: [
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 3702333),  # v1.3.0
+    ],
+    EthereumNetwork.ZILLIQA_EVM_TESTNET: [
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 6806685),  # v1.3.0
     ],
 }
