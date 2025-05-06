023dd1381d847c4f5763c6246ba394e0e8c5c746
Denis Smolonski <denissmolonski@gmail.com>
2025-05-06 11:52:13 +0200

    fix(safe): update for zilliqa

--- gnosis/safe/safe.py
+++ gnosis/safe/safe.py
@@ -580,6 +580,8 @@ class Safe(SafeCreator, ContractBase, metaclass=ABCMeta):
                 # NOTE: Zilliqa specific validation rules don't accept from_address caller itself or None.
                 #       So we use zero address as from_address.
                 from_address="0x0000000000000000000000000000000000000000",
+                # NOTE: Zilliqa RPC does not support multicall.
+                force_batch_call=True,
                 block_identifier=block_identifier,
                 raise_exception=False,
             )
